cb519456e64ea1b24dea44b7f30a700ca801eb10
Cakizy <58572124+Cakizy@users.noreply.github.com>
2020-03-28 03:39:28 +0100

    Homework CSharp 03- homeworks(tasks1)
    Homework CSharp 03- homeworks(tasks1)

---
+++ Homework CSharp 03- homeworks(tasks1)/Homework CSharp 03- homeworks(tasks1)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework_CSharp_03__homeworks_tasks1_
+{
+    class Program
+    {
+        static void Students(string[] message, int group)
+        {
+            Console.WriteLine("The Students in G{0} are:", group);
+
+            foreach (var item in message)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+            string[] studentsG2 = { "Caki", "Petar", "Martin", "Gordon", "Filip" };
+
+            Console.WriteLine("Please enter student group number: (1 or 2)");
+            bool input1 = Int32.TryParse(Console.ReadLine(), out int group);
+            while (group != 2 && group != 1)
+            {
+                Console.WriteLine("Whoops, something went wrong you need to input between 1 and 2 (G1 and G2)");
+                input1 = Int32.TryParse(Console.ReadLine(), out group);
+            }
+            if (group == 1) Students(studentsG1, group);
+            else Students(studentsG2, group);
+            Console.ReadLine();
+        }
+    }
+}
