70d7b6d7cfccb5b89aa6ce760f7ccd2a10baeca5
Cakizy <58572124+Cakizy@users.noreply.github.com>
2020-03-28 04:32:47 +0100

    Homework CSharp 04- homeworks(tasks3)
    Homework CSharp 04- homeworks(tasks3)

---
+++ Homework CSharp 04- homeworks(tasks3)/Homework CSharp 04- homeworks(tasks3)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework_CSharp_04__homeworks_tasks3_
+{
+    class Program
+    {
+        static int AgeCalculator(DateTime bDay)
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - bDay.Year;
+            return age;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter you're date of bday! (MM/DD/YYYY)");
+            bool userInput = DateTime.TryParse(Console.ReadLine(), out DateTime bday);
+            while (!userInput || bday > DateTime.Now)
+            {
+                Console.WriteLine("Enter correct birthday :/ (MM/DD/YYYY)");
+                userInput = DateTime.TryParse(Console.ReadLine(), out bday);
+            }
+            Console.WriteLine("Youre age: " + AgeCalculator(bday));
+            Console.ReadLine();
+        }
+    }
+}
