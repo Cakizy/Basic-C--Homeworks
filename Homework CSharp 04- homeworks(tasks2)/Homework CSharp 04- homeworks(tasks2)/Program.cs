6214a21ceb03a4a66e5a09f15c606410b655fc0b
Cakizy <58572124+Cakizy@users.noreply.github.com>
2020-03-28 04:19:50 +0100

    Homework CSharp 04- homeworks(tasks2)
    Homework CSharp 04- homeworks(tasks2)

---
+++ Homework CSharp 04- homeworks(tasks2)/Homework CSharp 04- homeworks(tasks2)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework_CSharp_04__homeworks_tasks2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+
+            Console.WriteLine("Tomorrow is " + today.AddDays(3));
+            Console.WriteLine("One month from now " + today.AddMonths(1));
+            Console.WriteLine("one month and 3 days from now " + today.AddDays(3).AddMonths(1));
+            Console.WriteLine("1 year and 2 months ago from today " + today.AddMonths(-2).AddYears(-1));
+            Console.WriteLine("Month " + today.Month);
+            Console.WriteLine("Year " + today.Year);
+            Console.ReadLine();
+        }
+    }
+}
